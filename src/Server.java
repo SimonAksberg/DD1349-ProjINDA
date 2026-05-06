1304eaabfd32e30fedb04598b6c2261f630e5d3f
Hanna <255732637+hazhng@users.noreply.github.com>
2026-05-06 15:53:58 +0200

    Add ability to create new lists (closes #16)

--- src/Server.java
+++ src/Server.java
@@ -1,12 +1,18 @@
 import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Server {
+
+    public static List<ToDoList> toDoLists = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
 
         server.createContext("/", new FrontEndHandler());
+
+        server.createContext("/api/lists", new ListHandler(toDoLists));
 
         server.start();
         System.out.println("Server running on http://localhost:8000");
