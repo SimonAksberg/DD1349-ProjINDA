c6d16b1419de9e473c05f9ac0a50f1cff8ecf846
Hanna <255732637+hazhng@users.noreply.github.com>
2026-05-06 15:18:51 +0200

    Implemented a basic server (closes #8)

---
+++ src/Server.java
@@ -0,0 +1,14 @@
+import com.sun.net.httpserver.HttpServer;
+import java.net.InetSocketAddress;
+
+public class Server {
+
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+
+        server.createContext("/", new FrontEndHandler());
+
+        server.start();
+        System.out.println("Server running on http://localhost:8000");
+    }
+}
