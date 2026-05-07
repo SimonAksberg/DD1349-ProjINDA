6f9d53847b9cb2a81ae234b09706434f26e4905e
Hanna <255732637+hazhng@users.noreply.github.com>
2026-05-07 15:33:10 +0200

    Implement backend to load lists in frontend

--- src/main/java/ListHandler.java
+++ src/main/java/ListHandler.java
@@ -3,11 +3,13 @@ import java.io.InputStream;
 import java.util.List;
 
 import com.sun.net.httpserver.HttpHandler;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 
 public class ListHandler implements HttpHandler {
 
     private List<ToDoList> lists;
+    private ObjectMapper mapper = new ObjectMapper();
 
     public ListHandler(List<ToDoList> lists) {
         this.lists = lists;
@@ -24,6 +26,15 @@ public class ListHandler implements HttpHandler {
 
             String response = "List added";
         
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        }
+
+        if (exchange.getRequestMethod().equals("GET")) {
+            String response = mapper.writeValueAsString(lists);
+                    
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
             exchange.sendResponseHeaders(200, response.getBytes().length);
             exchange.getResponseBody().write(response.getBytes());
             exchange.close();
